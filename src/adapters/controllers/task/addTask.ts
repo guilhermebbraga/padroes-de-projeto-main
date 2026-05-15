2dc2c37b569c69dc604bacf3c2cf807a938ee994
Guilherme Braga <bbraga.guilherme@gmail.com>
2026-05-15 11:07:17 -0300

    feat: implement validation for AddTaskController and create validation classes for required fields and date

--- src/adapters/controllers/task/addTask.ts
+++ src/adapters/controllers/task/addTask.ts
@@ -9,32 +9,25 @@ import {
 import { Controller } from "../../interfaces/controller";
 import { DateValidator } from "../../interfaces/dateValidator";
 import { AddTask } from "../../../usecases/addTask";
+import { Validation } from "../../interfaces/validation";
 
 export class AddTaskController implements Controller {
   constructor(
     private readonly addTask: AddTask,
-    private readonly dateValidator: DateValidator,
+    private readonly validation: Validation,
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ["title", "description", "date"];
+      const error = this.validation.validate(httpRequest.body);
 
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field));
-        }
+      if (error) {
+        return badRequest(error);
       }
+
       const { title, description, date } = httpRequest.body;
 
-      const isValid = this.dateValidator.isValid(date);
-
-      if (!isValid) {
-        return badRequest(new InvalidParamError("date"));
-      }
-
       const task = await this.addTask.add({ title, description, date });
-      // return created(task);
-      throw new Error("Forced Error");
+      return created(task);
     } catch (error: any) {
       return serverError(error);
     }
